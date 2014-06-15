4983ed46f7fb54c46c895c286c3ed2d0d0808e58
Devendra <devendra@udgama.com>
2014-06-15 13:00:16 +0530

    test page for ie 10 pam issue

--- web/unassembled/platform.js
+++ web/unassembled/platform.js
@@ -308,7 +308,7 @@ function ajax( setup ) {
             console.log(JSON.stringify(xhr));
             console.log(e);
             console.log(e.data);
-            console.log(e.data.responseText);
+            //console.log(e.data.responseText);
             console.log(xhr);
             done(1, xhr.responseText || { "error" : "Network Connection Error"})
         };
